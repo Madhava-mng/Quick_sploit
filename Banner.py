a3197fcbf5a66689d6c12702f3f847c91aa98872
MαđЋανα <madhava29082000@gmail.com>
2020-06-03 19:04:12 +0530

    banner

---
+++ Banner.py
@@ -0,0 +1,102 @@
+b1="""\u001b[36;1m
+                          `:oDFo:`
+                      ./ymM0dayMmy/.
+                   -+dHJ5aGFyZGVyIQ==+-
+                `:sm⏣~~Destroy.No.Data~~s:`
+            -+h2~~Maintain.No.Persistence~~h+-
+        `:odNo2~~Above.All.Else.Do.No.Harm~~Ndo:`
+    ./etc/shadow.0days-Data'%20OR%201=1--.No.0MN8'/.
+ -++SecKCoin++e.AMd`       `.-://///+hbove.913.ElsMNh+-
+-~/.ssh/id_rsa.Des-                  `htN01UserWroteMe!-
+:dopeAW.No<nano>o                     :is:TЯiKC.sudo-.A:
+:we're.all.alike'`                     The.PFYroy.No.D7:
+:PLACEDRINKHERE!:                      yxp_cmdshell.Ab0:
+:msf>exploit -j.                       :Ns.BOB&ALICEes7:
+:---srwxrwx:-.`                        `MS146.52.No.Per:
+:<script>.Ac816/                        sENbove3101.404:
+:NT_AUTHORITY.Do                        `T:/shSYSTEM-.N:
+:09.14.2011.raid                       /STFU|wall.No.Pr:
+:hevnsntSurb025N.                      dNVRGOING2GIVUUP:
+:#OUTHOUSE-  -s:                       /corykennedyData:
+:$nmap -oS                              SSo.6178306Ence:
+:Awsm.da:                            /shMTl#beats3o.No.:
+:Ring0:                             `dDestRoyREXKC3ta/M:
+:23d:                               sSETEC.ASTRONOMYist:
+ /-                        /yo-    .ence.N:(){ :|: & };:
+                           :Shall.We.Play.A.Game?tron/
+                          ```-ooy.if1ghtf0r+ehUser5`
+                        ..th3.H1V3.U2VjRFNN.jMh+.`
+                        `MjM~~WE.ARE.se~~MMjMs
+                        .. +~KANSAS.CITY's~-`
+                            J~HAKCERS~./.`
+                            .esc:wq!:`
+                             +++ATH`
+                              't/’
+
+\u001b[0m"""
+
+b2="""
+
+ {G}____________
+ [%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|{R} $a,{G}        |%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%]
+ [%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|{R} $S`?a,{G}     |%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%]
+ [%%%%%%%%%%%%%%%%%%%%__%%%%%%%%%%|{R}       `?a,{G} |%%%%%%%%__%%%%%%%%%__%%__ %%%%]
+ [% .--------..-----.|  |_ .---.-.|{R}       .,a$%{G}|.-----.|  |.-----.|__||  |_ %%]
+ [% |        ||  -__||   _||  _  ||{R}  ,,aS$""`  {G}||  _  ||  ||  _  ||  ||   _|%%]
+ [% |__|__|__||_____||____||___._||{R}%$P"`       {G}||   __||__||_____||__||____|%%]
+ [%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|{R} `"a,       {G}||__|%%%%%%%%%%%%%%%%%%%%%%%%%%]
+ [%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%|{R}____`"a,$$__{G}|%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%]
+ [%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% {R}       `"$   {G}%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%]
+ [%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%]
+
+\u001b[0m
+""".format(R="\u001b[31;1m",G="\u001b[36m")
+
+b3="""{R}
+
+                .;lxO0KXXXK0Oxl:.
+           ,o0WMMMMMMMMMMMMMMMMMMKd,
+        'xNMMMMMMMMMMMMMMMMMMMMMMMMMWx,
+      :KMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK:
+    .KMMMMMMMMMMMMMMMWNNNWMMMMMMMMMMMMMMMX,
+   lWMMMMMMMMMMMXd:..     ..;dKMMMMMMMMMMMMo
+  xMMMMMMMMMMWd.               .oNMMMMMMMMMMk
+ oMMMMMMMMMMx.                    dMMMMMMMMMMx
+.WMMMMMMMMM:                       :MMMMMMMMMM,
+xMMMMMMMMMo                         lMMMMMMMMMO
+NMMMMMMMMW                    ,cccccoMMMMMMMMMWlccccc;
+MMMMMMMMMX                     ;KMMMMMMMMMMMMMMMMMMX:
+NMMMMMMMMW.                      ;KMMMMMMMMMMMMMMX:
+xMMMMMMMMMd                        ,0MMMMMMMMMMK;
+.WMMMMMMMMMc                         'OMMMMMM0,
+ lMMMMMMMMMMk.                         .kMMO'
+  dMMMMMMMMMMWd'                         ..
+   cWMMMMMMMMMMMNxc'.              {N}  ########## {R}
+    .0MMMMMMMMMMMMMMMMWc           {N} #+#    #+#{R}
+      ;0MMMMMMMMMMMMMMMo.          {N}+:+{R}
+        .dNMMMMMMMMMMMMo          {N}+#++:++#+{R}
+           'oOWMMMMMMMMo          {N}      +:+{R}
+               .,cdkO0K;        {N}:+:    :+:{R}
+                                {N}:::::::+:{N}
+
+""".format(R="\u001b[31;1m",N="\u001b[0m")
+b4="""
+Metasploit Park, System Security Interface
+  Version Custom, Alpha E
+  Ready...
+  > access security
+  access: PERMISSION DENIED.
+  > access security grid
+  access: PERMISSION DENIED.
+  > access main security grid
+  access: PERMISSION DENIED....and...
+  \u001b[31;1mYOU DIDN'T SAY THE MAGIC WORD!
+  YOU DIDN'T SAY THE MAGIC WORD!
+  YOU DIDN'T SAY THE MAGIC WORD!
+  YOU DIDN'T SAY THE MAGIC WORD!
+  YOU DIDN'T SAY THE MAGIC WORD!
+  YOU DIDN'T SAY THE MAGIC WORD!
+  YOU DIDN'T SAY THE MAGIC WORD!\u001b[0m
+
+"""
+Blist=[b1,b1,b1]
