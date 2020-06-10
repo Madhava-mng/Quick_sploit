6a110309c602aa2f29bd7139a7cbb5885712ab69
MαđЋανα <madhava29082000@gmail.com>
2020-06-11 01:22:41 +0530

    modified

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 function check(){
     if [ -f /data/data/com.termux/files/usr/bin/ls ];then
         if [ -f /data/data/com.termux/files/usr/bin/msfconsole ];then
-            echo -e "[\033[32;1m+\u001b[0m] Check metasploit status ......................... Ok"
-            sleep 0.8
+            printf "[\033[32;1m+\u001b[0m] Check metasploit status ...................... Ok\n"
+            sleep 0.4
         else
-            echo -e "[\033[31;1m!\u001b[0m] Check metasploit status ......................... Installing"
+            printf "[\033[31;1m!\u001b[0m] Check metasploit status ......................... Installing\n"
             echo "\nPleace be wite.\n Turn on Data.\n\n"
             sleep 1
             pkg install unstable-repo && pkg install metasploit 2>/dev/null
@@ -13,24 +13,40 @@ function check(){
             check
         fi
         if [ -f /data/data/com.termux/files/usr/bin/python ];then
-            echo -e "[\033[32;1m+\u001b[0m] Check Python3 status ......................... Ok"
-            sleep 1
+            printf  "[\033[32;1m+\u001b[0m] Check Python3 status ......................... Ok\n"
+            sleep 0.5
         else
-            echo -e "[\033[31;1m!\u001b[0m] Check Python3 status ......................... Installing"
+            printf "[\033[31;1m!\u001b[0m] Check Python3 status ......................... Installing\n"
             sleep 1
             pkg install python 2>/dev/null
             check
         fi
     else
         if [ -f /usr/bin/msfconsole ];then
-            echo -e "[\033[32;1m+\u001b[0m] Check metasploit status ......................... Ok"
+            printf  "[\033[32;1m+\u001b[0m] Check metasploit status ...................... Ok\n"
             sleep 1
         else
-            echo -e "[\033[31;1m!\u001b[0m] Check metasploit status ......................... Installing"
+            printf  "[\033[31;1m!\u001b[0m] Check metasploit status ......................... Installing\n"
             echo "\nPleace be wite.\n Turn on Data.\n\n"
             sleep 1
             sudo apt-get install metasploit-framework 2>/dev/null
         fi
     fi
 }
+function check2(){
+    path="`pwd`/module.txt"
+    if [ -f $path ];then
+        printf "[\033[32;1m+\u001b[0m] Check modules status ......................... Ok\n"
+        sleep 0.5
+    else
+        printf "[\033[31;1m!\u001b[0m] Check modules status ......................... \033[31;1mFail\033[0m\n"
+    fi
+    if [ -f "`pwd`/Banner.py" ];then
+        printf "[\033[32;1m+\u001b[0m] Check banners status ......................... Ok\n"
+        sleep 1
+    else
+        printf "[\033[31;1m!\u001b[0m] Check banners status ......................... \033[31;1mFail\033[0m\n"
+    fi
+}
 check
+check2
